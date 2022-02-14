4e8ba60580c9f3a0c75d17b4800b25b8875e06b9
Tsuyoshi Ushio <ushio@simplearchitect.com>
2022-02-13 18:05:51 -0800

    Add project files.

---
+++ SingularToPlural/StringExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SingularToPlural
+{
+    public static class StringExtensions
+    {
+        private static readonly ConcurrentDictionary<string, string> _cache = new ConcurrentDictionary<string, string>();
+        public static string Pluralize(this string word)
+        {
+            if (_cache.ContainsKey(word)) return _cache[word];
+
+            string replaced;
+            if (TryReplace(word, "s$", "s", out replaced)) return UpdateCache(word, replaced);
+            if (TryReplace(word, "^(ax|test)is$", "$1es", out replaced)) return UpdateCache(word, replaced);
+            if (TryReplace(word, "(octop|vir)i$", "$1i", out replaced)) return UpdateCache(word, replaced);
+
+            if (string.Compare(replaced, word) == 0)
+            {
+                replaced = Regex.Replace(word, "$", "s");
+                return UpdateCache(word, replaced);
+            }
+            return word;
+        }
+
+        private static string UpdateCache(string word, string replaced)
+        {
+            return _cache.GetOrAdd(word, replaced);
+        }
+
+        private static bool TryReplace(string word, string pattern, string replacement, out string replaced)
+        {
+            Regex rgx = new Regex(pattern);
+            replaced = rgx.Replace(word, replacement);
+            return rgx.IsMatch(word);
+        }
+    }
+}
